1b05f76de128a4d69f6205ce8293090d5d2f1538
イリーナ・コバルチュク <foaxdev@yahoo.com>
2020-02-23 22:43:35 +0300

    Initialize basic redux structure

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,21 @@ import ReactDOM from "react-dom";
 import React from "react";
 import {App} from "./components/app/app";
 import {offers, offersNearby} from "./mocks/offers";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./reducer";
 
 const HEADER_HOVER_CARD_HANDLER = () => {};
 const CITY_COORDINATES = [52.38333, 4.9];
 const OFFER_COORDINATES = [52.3709553943508, 4.89309666406198];
 
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
+
 ReactDOM.render(
+  <Provider store={store}>
     <App
       cityCoordinates={CITY_COORDINATES}
       offerCoordinates={OFFER_COORDINATES}
@@ -16,5 +25,6 @@ ReactDOM.render(
       onCardHover={HEADER_HOVER_CARD_HANDLER}
       onOfferCardHover={HEADER_HOVER_CARD_HANDLER}
     />,
-    document.getElementById(`root`)
+  </Provider>,
+  document.querySelector(`#root`)
 );
